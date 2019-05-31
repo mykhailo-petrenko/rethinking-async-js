03c4d65f83a6e2da8971f54c7063ba081e0269c1
Mykhailo Petrenko <mihalytch@gmail.com>
2019-05-31 23:24:37 +0300

    Ex2. Lazy Thunk

--- exercises/ex2/ex2.js
+++ exercises/ex2/ex2.js
@@ -20,8 +20,38 @@ function output(text) {
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+	let subscribe;
+	let responce;
+
+	fakeAjax(file, (text) => {
+		if (subscribe === undefined) {
+			responce = text;
+		} else {
+			subscribe(text);
+		}
+	});
+
+	return function(cb) {
+		if (responce === undefined) {
+			subscribe = cb;
+		} else {
+			cb(responce);
+		}
+	};
 }
 
 // request all files at once in "parallel"
-// ???
+
+const f1 = getFile('file1');
+const f2 = getFile('file2');
+const f3 = getFile('file3');
+
+f1((text1) => {
+	output(text1);
+	f2((text2) => {
+		output(text2);
+		f3((text3) => {
+			output(text3);
+		});
+	});
+});
